7009c4c38d432cb12b747f4eab844a97cc21d984
榑林晃平 <kureykou321@gmail.com>
2020-03-29 01:11:37 +0900

    fix-home-controller

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,60 +1,55 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
 
-import ch.qos.logback.classic.spi.STEUtil;
-
-@RestController
-
+@Controller
+// controllerのアノテーション
 public class HomeController {
-
-	String[] names = { "tuyono", "hanako", "taro", "sachiko", "ichiro" };
-
-	String[] mails = { "tuyono.com", "hanako@flower", "taro@yamada", "sachiko@happy", "ichiro@baseball" };
-
-	@RequestMapping("/{id}")
-	public Dataobject index(@PathVariable int id) {
-		return new Dataobject(id, names[id], mails[id]);
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		return mav;
 	}
 
-	class Dataobject {
-		// privateは、外部のクラスから値を変更できないように、アクセスの宣言している
-		private int id;
-		private String name;
-		private String value;
+	@RequestMapping("/other")
+	public String other() {
+		return "refirect:/";
+	}
 
-		public Dataobject(int id, String name, String value) {
-			// superは親クラスのコンストラクタ
-			super();
-			this.id = id;
-			this.name = name;
-			this.value = value;
-		}
+	@RequestMapping("/home")
+	public String home() {
+		return "forword:/";
+	}
 
-		public int getId() {
-			return id;
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public ModelAndView send(
+	  // valueがパラメーターで受け取る名前、
+	  @RequestParam(value="check1",required=false)boolean check1,
+	  @RequestParam(value="radio1",required=false)String radio1,
+	  @RequestParam(value="select1",required=false)String select1,
+	  @RequestParam(value="select2",required=false)String[] select2,
+	  ModelAndView mav
+	) {
+	  String res = "";
+	  try {
+		res = "check1:" + check1 +
+		"radio:" + radio1 +
+		" select:" + select1 +
+		" \nselect2:";
+		  } catch(NullPointerException e) {}
+		try {
+		  res += select2[0];
+		  for(int i = 0; i < select2.length;i++)
+		  res += "," + select2[i];	
+		} catch(NullPointerException e) {
+		  res += "null";
 		}
-
-		public void setId(int id) {
-			this.id = id;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
+	  mav.addObject("msg",res);
+	  mav.setViewName("index");
+	  return mav;
 	}
 }
