453f923200242b791205748471a25d038311eaaa
榑林晃平 <kureykou321@gmail.com>
2020-03-31 20:10:23 +0900

    message

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -15,41 +13,9 @@ public class HomeController {
 		return mav;
 	}
 
-	@RequestMapping("/other")
-	public String other() {
-		return "refirect:/";
-	}
-
 	@RequestMapping("/home")
 	public String home() {
-		return "forword:/";
+		return "forward:/";
 	}
-
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public ModelAndView send(
-	  // valueがパラメーターで受け取る名前、
-	  @RequestParam(value="check1",required=false)boolean check1,
-	  @RequestParam(value="radio1",required=false)String radio1,
-	  @RequestParam(value="select1",required=false)String select1,
-	  @RequestParam(value="select2",required=false)String[] select2,
-	  ModelAndView mav
-	) {
-	  String res = "";
-	  try {
-		res = "check1:" + check1 +
-		"radio:" + radio1 +
-		" select:" + select1 +
-		" \nselect2:";
-		  } catch(NullPointerException e) {}
-		try {
-		  res += select2[0];
-		  for(int i = 0; i < select2.length;i++)
-		  res += "," + select2[i];	
-		} catch(NullPointerException e) {
-		  res += "null";
-		}
-	  mav.addObject("msg",res);
-	  mav.setViewName("index");
-	  return mav;
-	}
+	
 }
